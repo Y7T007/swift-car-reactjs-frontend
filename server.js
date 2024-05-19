6930825721df03c79f8986d6907dfa1e6669b4e3
Y7T007 <yassir7t@gmail.com>
2024-05-19 06:21:36 +0100

    Updating the port to deploy the full app

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
 });
